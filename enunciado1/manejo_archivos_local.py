fb2fdca5767b9ae93279d8709b5581b570de0fff
LuisaFRojas <luisaferrojas95@gmail.com>
2025-05-20 19:15:05 -0500

    Add all nedded files

---
+++ enunciado1/manejo_archivos_local.py
@@ -0,0 +1,46 @@
+
+"""
+Objetivo: Automatizar tareas básicas de manejo de archivos localmente.
+Enunciado:
+
+1. Cree una carpeta llamada `reportes/` si no existe.
+2. Dentro de esa carpeta, genere un archivo de texto llamado `reporte_<fecha>.txt`, donde <fecha> esté en
+   formato YYYYMMDD.
+3. El archivo debe contener:
+
+* La lista de todos los archivos `.txt` que existan en la carpeta actual (donde se ejecuta el script).
+* Para cada archivo listado, mostrar su nombre y el número de líneas que contiene.
+
+4. Al final del archivo de reporte, incluir la cantidad total de archivos `.txt` procesados.
+   Extra: Si no se encuentra ningún archivo`.txt`, el script debe escribir en el reporte: “No se encontraron
+   archivos de texto.”
+
+"""
+import datetime
+import os
+
+#1 Crear carpeta si la carpeta no existe
+carpeta_reporte = 'reportes'
+#Uso la library de OS para interactuar con el sistema operativo - Si la carpeta ya existe no arroja error con el uso de 'exist_ok'
+os.makedirs(carpeta_reporte,exist_ok=True)
+
+#2 genere un archivo de texto llamado `reporte_<fecha>.txt` con formato YYYYMMDD.
+fecha_hoy = (datetime.datetime.now().strftime('%Y%m%d'))
+reporte = f'reporte_{fecha_hoy}.txt'
+path_reporte = os.path.join(carpeta_reporte, reporte)
+#Generar el nombre del reporte con extension txt y crear el path donde se va a guardar el archivo
+
+#3 Generar una lista de todos los archivos `.txt` que existan en la carpeta
+
+lista_archivos = [i for i in os.listdir('reportes') if i.endswith('.txt')]
+#Popular la lista basada en la condicion que el archivo sea extension .txt y que el path sea valido
+
+#4 Generar el reporte y escribir el numero total de archivos encontrados
+print(lista_archivos)
+with open(path_reporte, 'w', encoding='utf-8') as reporte_final:
+    # Nos deja sobreescribir el archivo si ya existe
+    if lista_archivos:
+        reporte_final.write(f'Total de archivos .txt en la carpeta: {len(lista_archivos)}')
+    else:
+        reporte_final.write('No se encontraron archivos de texto.')
+print(f'Encuentra el reporte en:  {path_reporte}')
---
+++ enunciado2/postgresql.py
@@ -0,0 +1,62 @@
+"""
+Base de datos PostgreSQL
+Objetivo: Consultar e insertar datos en una base de datos PostgreSQL.
+Enunciado:
+1. Crea una tabla llamada `usuarios` con los siguientes campos:
+
+id SERIAL PRIMARY KEY,
+nombre VARCHAR(100),
+correo VARCHAR(100),
+fecha_registro TIMESTAMP DEFAULT NOW()
+
+2. Inserta al menos 3 usuarios con datos ficticios.
+3. Escribe una consulta para obtener todos los usuarios registrados en los últimos 7 días.
+4. En Python, usando `psycopg2` o `SQLAlchemy`, crea un script que:
+- Inserte un nuevo usuario desde variables del código.
+- Lea y muestre por consola todos los usuarios en la base de datos.
+"""
+from itertools import count
+
+import psycopg2
+
+#1 Hacer conexion con la base de datos
+conexion = psycopg2.connect(dbname='postgres',user='postgres',password='',host="localhost",port="5432")
+
+try:
+    with conexion:
+        with conexion.cursor() as selector:
+            #crear tabla
+            selector.execute(""" 
+            CREATE TABLE IF NOT EXISTS usuarios ( id SERIAL PRIMARY KEY, 
+                                                nombre VARCHAR(100), 
+                                                correo VARCHAR(100),
+                                                fecha_registro TIMESTAMP DEFAULT NOW()
+                                                );""")
+
+            #Insertar 3 datos
+            selector.executemany("""INSERT INTO usuarios (nombre,correo) VALUES (%s,%s);""",
+                             [('Pepito perez', 'pepito_perez55@unmail.com'),
+                              ('Luisa Rodriguez','luisa_rod95@unmail.com'),
+                              ('Michelle Juarez','mich-juarez@unmail.com')])
+
+            #Seleccionar los usuarios registrados en los ultimos 7 dias
+            print('Usuarios registrados en los ultimos 7 dias: \n')
+            selector.execute("""SELECT id, nombre, correo, fecha_registro FROM usuarios WHERE fecha_registro >= NOW() - INTERVAL '7 days';""")
+            usuarios_ultimos_7dias= selector.fetchall()
+            for registrado in usuarios_ultimos_7dias:
+                print(f"ID: {registrado[0]}, Nombre: {registrado[1]}, Correo: {registrado[2]}, Fecha: {registrado[3]}")
+
+            #Insertar datos desde una variable
+            nombre_nuevo_usuario = "Camilo Andrade"
+            correo_nuevo_usuario = "andrade-camilo@unmail.com"
+            selector.execute("""INSERT INTO usuarios (nombre,correo) VALUES (%s,%s);""",(nombre_nuevo_usuario,correo_nuevo_usuario))
+
+            # Seleccionar todos los usuarios en la base de datos
+            print("Todos los usuarios en la base de datos: \n")
+            selector.execute("SELECT id, nombre, correo, fecha_registro FROM usuarios;")
+            usuarios = selector.fetchall()
+            for usuario in usuarios:
+                print(f"ID: {usuario[0]}, Nombre: {usuario[1]}, Correo: {usuario[2]}, Fecha: {usuario[3]}")
+finally:
+    #Cerrar la conexion a la base de datos al finalizar sin importar que se haya generado algun error.
+    conexion.close()
